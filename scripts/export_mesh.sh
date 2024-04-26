779155261374ee8759e67bf2ce78fd1e9b5e6025
themestation <zwwang96@gmail.com>
2024-04-27 02:53:59 +0800

    update code

--- scripts/export_mesh.sh
+++ scripts/export_mesh.sh
@@ -4,4 +4,4 @@ export DIFFUSERS_OFFLINE=$OFFLINE_MODE
 export HF_HUB_OFFLINE=$OFFLINE_MODE
 
 # export mesh
-python run.py --test_img_dir data/concept_images/elevation_20 --obj_name owl_0 --gpu_ids 0 --export_mesh --task_name demo --seed 42 --port 30000
+python run.py --test_img_dir data/concept_images/elevation_20 --obj_name owl_1 --gpu_ids 0 --export_mesh --task_name demo --seed 42 --port 30000
--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,4 @@ export DIFFUSERS_OFFLINE=$OFFLINE_MODE
 export HF_HUB_OFFLINE=$OFFLINE_MODE
 
 # run a single case
-python run.py --test_img_dir data/concept_images/elevation_20 --obj_name owl_freepik-reimagine --gpu_ids 0 --get_multi_view --get_coarse_3d --train_dreambooth --get_optim_3d  --task_name demo --seed 42 --port 30000 --remove_dreambooth_ckpt
-# python run.py --test_img_dir data/concept_images/elevation_20 --obj_name owl_1 --gpu_ids 0 --get_multi_view --get_coarse_3d --train_dreambooth --get_optim_3d  --task_name demo --seed 42 --port 30000 --remove_dreambooth_ckpt
+python run.py --test_img_dir data/concept_images/elevation_20 --obj_name owl_1 --gpu_ids 0 --get_multi_view --get_coarse_3d --train_dreambooth --get_optim_3d  --task_name demo --seed 42 --port 30000 --remove_dreambooth_ckpt
